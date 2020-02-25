1cb0d0c9ab99e4bc71f36fc48350a3719422caf8
Erlang Parasu <erlangparasu@gmail.com>
2020-02-25 12:55:55 +0700

    Change the UI using Themes

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
     );
   }
 }
